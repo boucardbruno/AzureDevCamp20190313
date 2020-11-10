23623d411570cdcded5c1cae2304a4ae6aa61a26
Bruno Boucard <boucard.bruno@free.fr>
2020-11-10 16:45:56 +0100

    Ajout de nouvelles règles

--- TrainTrain.Test/Specification/Features/BookTrainTicket.feature
+++ TrainTrain.Test/Specification/Features/BookTrainTicket.feature
@@ -3,21 +3,31 @@
 
 Rule: For a train overall, no more than 70% of seats may be reserved in advance
 
-Scenario: No more than 70% of seats may be reserved in advance for a train overall, 
-	Given one coach empty made of 10 seats
-	When 3 seats are requested
-	Then the reservation should be assigned these seats "1A, 2A, 3A"
+	Scenario: No more than 70% of seats may be reserved in advance for a train overall, 
+		Given one coach empty made of 10 seats
+		When 3 seats are requested
+		Then the reservation should be assigned these seats "1A, 2A, 3A"
 
-Rule: Ideally no individual coach should have no more than 70% reserved seats
+	Scenario: Not reserve seats when it exceeds train max capacity 70%
+		Given 1 coaches of 10 seats and 6 already reserved
+		When 3 seats are requested
+		Then the reservation should be failed
 
-Scenario: Not reserve seats when it exceeds train max capacity 70%
-	Given 1 coaches of 10 seats and 6 already reserved
-	When 3 seats are requested
-	Then the reservation should be failed
+Rule: Ideally no individual coach must have no more than 70% reserved seats
 
-Rule: Must put all the seats for one reservation in the same coach
+	Scenario: Max 70% reserved seats in one coach
+		Given 1 coaches of 10 seats and 5 already reserved
+		When 2 seats are requested
+		Then the reservation should be assigned these seats "6A, 7A"
 
-Scenario: Each reservation must be booked in the same coach
-	Given 2 coaches of 10 seats and 9 seats already reserved in the coach 1
-	When 2 seats are requested
-	Then the reservation should be assigned these seats "1B, 2B"
+	Scenario: Max 70% reserved seats for all coaches
+		Given 2 coaches of 10 seats and 5 seats already reserved in the coach 1
+		When 3 seats are requested
+		Then the reservation should be assigned these seats "1B, 2B, 3B"
+
+Rule: All seats for one reservation in the same coach
+
+	Scenario: Each reservation must be booked in the same coach
+		Given 2 coaches of 10 seats and 6 seats already reserved in the coach 1
+		When 2 seats are requested
+		Then the reservation should be assigned these seats "1B, 2B"
--- TrainTrain.Test/Specification/StepDefinition/TrainReservationSteps.cs
+++ TrainTrain.Test/Specification/StepDefinition/TrainReservationSteps.cs
@@ -17,6 +17,8 @@ namespace TrainTrain.Test.Specification.StepDefinition
     {
         private readonly BookingReference _bookingReference = new BookingReference("341RTFA");
         private readonly TrainId _trainId = new TrainId("9043-2019-03-13");
+        private IProvideTrainTopology _provideTrainTopology;
+        private int _seatsCountRequested;
 
         private IProvideTicket TicketOffice { get; set; }
         private Reservation Reservation { get; set; }
@@ -24,61 +26,64 @@ namespace TrainTrain.Test.Specification.StepDefinition
         [Given(@"one coach empty made of (.*) seats")]
         public void GivenOneCoachEmptyMadeOfSeats(int availableSeats)
         {
-            var expectedSeats = "A1, A2, A3";
-            var provideTrainTopology = BuildTrainTopology(_trainId,
+            _provideTrainTopology = BuildTrainTopology(_trainId,
                 TrainTopologyGenerator.With_n_seats_and_m_already_reserved(availableSeats, 1));
-            var bookingReferenceService = BuildBookingReferenceService(_bookingReference);
-            var provideReservationService = BuildMakeReservation(_trainId, _bookingReference, expectedSeats);
-
-            TicketOffice =
-                new TicketOfficeService(provideTrainTopology, provideReservationService, bookingReferenceService);
         }
 
         [Given(@"(.*) coaches of (.*) seats and (.*) already reserved")]
         public void GivenCoachesOfSeatsAndAlreadyReserved(int coachesCount, int seatsCount, int alreadyReservedSeats)
         {
-            var trainTopology = BuildTrainTopology(_trainId,
+             _provideTrainTopology = BuildTrainTopology(_trainId,
                 TrainTopologyGenerator.With_n_seats_and_m_already_reserved(seatsCount, coachesCount,
                     alreadyReservedSeats, _bookingReference.Id));
-            var bookingReferenceService = BuildBookingReferenceService(_bookingReference);
-            var provideReservationService = BuildMakeReservation(_trainId, _bookingReference, "A1, A2, A3");
 
-            TicketOffice = new TicketOfficeService(trainTopology, provideReservationService, bookingReferenceService);
         }
 
         [Given(@"(.*) coaches of (.*) seats and (.*) seats already reserved in the coach (.*)")]
         public void GivenCoachesOfSeatsAndSeatsAlreadyReservedInTheCoach(int coachesCount, int seatsCount,
             int alreadyReservedSeats, int coachNumber)
         {
-            var provideTrainTopology = BuildTrainTopology(_trainId,
+            _provideTrainTopology = BuildTrainTopology(_trainId,
                 TrainTopologyGenerator.With_n_seats_and_m_already_reserved(seatsCount, coachesCount,
                     alreadyReservedSeats, _bookingReference.Id, coachNumber));
-            var provideReservationService =
-                BuildMakeReservation(_trainId, _bookingReference, "B1, B2");
-            var bookingReferenceService = BuildBookingReferenceService(_bookingReference);
-
-            TicketOffice =
-                new TicketOfficeService(provideTrainTopology, provideReservationService, bookingReferenceService);
         }
 
         [When(@"(.*) seats are requested")]
-        public async Task WhenSeatsAreRequested(int seatsCountRequested)
+        public void WhenSeatsAreRequested(int seatsCountRequested)
         {
-            Reservation = await TicketOffice.Reserve(_trainId, new SeatsRequested(seatsCountRequested));
+            _seatsCountRequested = seatsCountRequested;
         }
 
         [Then(@"the reservation should be assigned these seats ""(.*)""")]
-        public void ThenTheReservationShouldBeAssignedTheseSeats(string expectedSeats)
+        public async Task ThenTheReservationShouldBeAssignedTheseSeats(string expectedSeats)
         {
             var seatsAsString = string.Join(", ", expectedSeats.Split(",").Select(s => $"\"{s.Trim()}\""));
+
+            var bookingReferenceService = BuildBookingReferenceService(_bookingReference);
+            var provideReservationService = BuildMakeReservation(_trainId, _bookingReference, expectedSeats);
+
+            TicketOffice =
+                new TicketOfficeService(_provideTrainTopology, provideReservationService, bookingReferenceService);
+
+            Reservation = await TicketOffice.Reserve(_trainId, new SeatsRequested(_seatsCountRequested));
+
+
             Check.That(SeatsReservationAdapter.AdaptReservation(Reservation))
                 .IsEqualTo(
                     $"{{\"train_id\": \"{_trainId}\", \"booking_reference\": \"{_bookingReference}\", \"seats\": [{seatsAsString}]}}");
         }
 
         [Then(@"the reservation should be failed")]
-        public void ThenTheReservationShouldBeFailed()
+        public async Task ThenTheReservationShouldBeFailed()
         {
+            var bookingReferenceService = BuildBookingReferenceService(_bookingReference);
+            var provideReservationService = BuildMakeReservation(_trainId, _bookingReference, "");
+
+            TicketOffice =
+                new TicketOfficeService(_provideTrainTopology, provideReservationService, bookingReferenceService);
+
+            Reservation = await TicketOffice.Reserve(_trainId, new SeatsRequested(_seatsCountRequested));
+
             Check.That(SeatsReservationAdapter.AdaptReservation(Reservation))
                 .IsEqualTo($"{{\"train_id\": \"{_trainId}\", \"booking_reference\": \"\", \"seats\": []}}");
         }
@@ -114,6 +119,9 @@ namespace TrainTrain.Test.Specification.StepDefinition
         private static IEnumerable<Seat> ToSeats(string expectedSeats)
         {
             var seats = new List<Seat>();
+            
+            if (expectedSeats == string.Empty) return seats;
+
             foreach (var seatName in expectedSeats.Split(','))
             {
                 var (coachName, seatNumber) = ToSeat(seatName.Trim());
@@ -124,7 +132,8 @@ namespace TrainTrain.Test.Specification.StepDefinition
 
         private static (string coachName, int seatNumber) ToSeat(string seatAsString)
         {
-            return (seatAsString[0].ToString(), Convert.ToInt32(seatAsString.Substring(1)));
+            var value = seatAsString[0].ToString();
+            return (seatAsString[1].ToString(), Convert.ToInt32(value));
         }
     }
 }
