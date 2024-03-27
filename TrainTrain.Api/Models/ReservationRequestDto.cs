c765f2e59aec71bb492b0a4f31df8669e26904b8
Bruno Boucard <boucard.bruno@free.fr>
2024-03-27 13:15:20 +0100

    Migration to .NET 8.0

--- TrainTrain.Api/Models/ReservationRequestDto.cs
+++ TrainTrain.Api/Models/ReservationRequestDto.cs
@@ -7,6 +7,11 @@ namespace TrainTrain.Api.Models
 {
     public class ReservationRequestDto
     {
+        public ReservationRequestDto(string trainId, int numberOfSeats)
+        {
+            train_id = trainId;
+            number_of_seats = numberOfSeats;
+        }
         public string train_id { get; set; }
         public int number_of_seats { get; set; }
     }
